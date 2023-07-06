5eec9193ef94ecc9d148fd711c62998db257eb0d
Mario Mifsud <mario.p.mifsud@gmail.com>
2023-07-06 17:41:29 +1000

    Device summary cache and fake devices registrar components

---
+++ MM.EagleRock.Business/Devices/DeviceSummaryCache.cs
@@ -0,0 +1,48 @@
+﻿using MM.EagleRock.Contract;
+using MM.EagleRock.Contract.Cache;
+using MM.EagleRock.Contract.Models;
+
+namespace MM.EagleRock.Business.Devices
+{
+    /// <summary>
+    /// Devices summary caching component.
+    /// </summary>
+    /// <remarks>
+    /// TODO: Add unit test coverage.
+    /// </remarks>
+    public class DeviceSummaryCache : IDeviceSummaryCache
+    {
+        private ICacheService _cacheService;
+
+        public DeviceSummaryCache(ICacheService cacheService) 
+        {
+            _cacheService = cacheService;
+        }
+
+        /// <inheritdoc/>
+        public IDictionary<Guid, DeviceSummary> GetDeviceSummaries(IEnumerable<Guid> deviceIds)
+        {
+            return deviceIds
+                .Select(deviceId => 
+                    {
+                        // TODO: Evaluate either retrieving values from cache for all device Ids in batch, or
+                        //  parallelizing per-device cached value retrievals
+                        var lastTrafficUpdate = _cacheService.Get<RoadTrafficUpdatePayload>(deviceId.ToString());
+
+                        return new DeviceSummary
+                        { 
+                            DeviceId = deviceId, 
+                            Status = lastTrafficUpdate != null ? DeviceStatus.Active : DeviceStatus.Unknown,
+                            LatestRoadTrafficUpdate = lastTrafficUpdate
+                        };
+                    })
+                .ToDictionary(summary => summary.DeviceId, summary => summary);
+        }
+
+        /// <inheritdoc/>
+        public void PublishRoadTrafficUpdate(RoadTrafficUpdatePayload trafficUpdate)
+        {
+            _cacheService.Set(trafficUpdate.DeviceId.ToString(), trafficUpdate);
+        }
+    }
+}
---
+++ MM.EagleRock.Business/Devices/FakeDeviceRegistrar.cs
@@ -0,0 +1,32 @@
+﻿using MM.EagleRock.Contract;
+
+namespace MM.EagleRock.Business.Devices
+{
+    /// <summary>
+    /// A stubbed device registrar, using a list of static fake device IDs.
+    /// </summary>
+    /// <remarks>
+    /// TODO: Replace with data-driven devices registrar.
+    /// </remarks>
+    public class FakeDeviceRegistrar : IDeviceRegistrar
+    {
+        private static IEnumerable<Guid> REGISTERED_FAKE_DEVICE_IDS = new HashSet<Guid> 
+        {
+            new Guid("cda4687e-b8bc-4a50-a9d1-00ef4007e8ca"),
+            new Guid("c4567668-5073-41d6-a633-13e5049e7775"),
+            new Guid("71b3978d-2a35-4314-9908-ee5fa704caac")
+        };
+
+        /// <inheritdoc/>
+        public IEnumerable<Guid> GetRegisteredDeviceIds()
+        {
+            return REGISTERED_FAKE_DEVICE_IDS.AsEnumerable();
+        }
+
+        /// <inheritdoc/>
+        public bool IsDeviceRegistered(Guid deviceId)
+        {
+            return REGISTERED_FAKE_DEVICE_IDS.Contains(deviceId);
+        }
+    }
+}
