3f1c8063b1d4f2d831ba37a49da9fe8d5176efbf
Mario Mifsud <mario.p.mifsud@gmail.com>
2023-07-06 17:30:06 +1000

    DAL project with simple Redis caching service

---
+++ MM.EagleRock.Contract/Cache/ICacheService.cs
@@ -0,0 +1,25 @@
+﻿namespace MM.EagleRock.Contract.Cache
+{
+    /// <summary>
+    /// Simple contract for a data item caching service.
+    /// </summary>
+    public interface ICacheService
+    {
+        /// <summary>
+        /// Attempts to get item from cache using the supplied key.
+        /// </summary>
+        /// <typeparam name="T">Type of item to de-serialize to</typeparam>
+        /// <param name="key">Cache item key</param>
+        /// <returns>Cached item, de-serialized to type <see cref="T"/></returns>
+        T? Get<T>(string key);
+
+        /// <summary>
+        /// Caches supplied item with supplied key.
+        /// </summary>
+        /// <typeparam name="T">Type of item to serialize from</typeparam>
+        /// <param name="key">Cache item key</param>
+        /// <param name="value">Cache item</param>
+        /// <returns>Same item once cached</returns>
+        T Set<T>(string key, T value);
+    }
+}
---
+++ MM.EagleRock.DAL/Cache/RedisCacheService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using MM.EagleRock.Contract.Cache;
+using System.Text.Json;
+
+namespace MM.EagleRock.DAL.Cache
+{
+    /// <summary>
+    /// A simple cachine service using Redis.
+    /// </summary>
+    /// <remarks>
+    /// TODO: Add unit test coverage
+    /// </remarks>
+    public class RedisCacheService : ICacheService
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisCacheService(IDistributedCache cache) 
+        {
+            _cache = cache;
+        }
+
+        /// <inheritdoc/>
+        public T? Get<T>(string key)
+        {
+            var value = _cache.GetString(key);
+
+            if (value != null) 
+            {
+                return JsonSerializer.Deserialize<T>(value);
+            }
+
+            return default;
+        }
+
+        /// <inheritdoc/>
+        public T Set<T>(string key, T value)
+        {
+            // TODO: drive cache item expiration options via service configuration, or through custom logic
+            var timeOut = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10),
+                SlidingExpiration = TimeSpan.FromMinutes(1)
+            };
+
+            _cache.SetString(key, JsonSerializer.Serialize(value), timeOut);
+
+            return value;
+        }
+    }
+}
